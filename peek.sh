658fa8ca2b00f33b8b774b67649813129444d86e
Lorena <lorenavc@github.com>
2023-11-02 00:44:37 +0100

    forth commit

--- peek.sh
+++ peek.sh
@@ -2,6 +2,12 @@ if [[ -z $2 ]];
 then lines=3 
 else lines=$2
 fi
+filelines=$( cat $1 | wc -l)
+if [[ $filelines -lt $lines ]];
+then cat $1
+else
+echo warning
 head -n $lines $1
 echo "..."
 tail -n $lines $1
+fi
