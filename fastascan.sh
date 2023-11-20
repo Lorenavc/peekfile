f6933d1add23fb998d9fbde007fa0dfcad4ba017
Lorena <lorenavc@github.com>
2023-11-20 10:40:50 +0100

    second commit

--- fastascan.sh
+++ fastascan.sh
@@ -8,4 +8,17 @@ fi
 
 #Finding the number of fasta/fa files
 NUMFILES=$(find $X -type f -name "*.fa" -o -name "*.fasta" | wc -l)
+
+##Checking if there are no fasta files
+if [[ $NUMFILES -eq 0 ]]; then
+echo No fasta/fa files found. Exiting 
+exit 1
+fi
+
 echo Number of fasta/fa files: $NUMFILES
+
+
+#Finding the total number of unique fasta IDs
+FILES=$(find $X -type f -name "*.fa" -o -name "*.fasta")
+NUMOFIDS=$(awk '/>/{print $1}' $FILES | sort | uniq | wc -l) 
+echo Total number of unique fasta IDs: $NUMOFIDS
