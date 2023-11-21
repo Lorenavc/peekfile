8d7e5c899b9e0c000a95612c72131014cec71dff
Lorena <lorenavc@github.com>
2023-11-21 17:40:02 +0100

    third commit

--- fastascan.sh
+++ fastascan.sh
@@ -22,3 +22,32 @@ echo Number of fasta/fa files: $NUMFILES
 FILES=$(find $X -type f -name "*.fa" -o -name "*.fasta")
 NUMOFIDS=$(awk '/>/{print $1}' $FILES | sort | uniq | wc -l) 
 echo Total number of unique fasta IDs: $NUMOFIDS
+
+#Iterating through fasta/fa files
+find $X -type f -name "*.fa" -o -name "*.fasta" | while read i 
+do 
+
+##Check if file is a symbolic link
+if [[ -h $i ]]
+then FILETYPE="symlink"
+else
+     FILETYPE="non-symlink"
+fi
+
+##Count the number of fasta sequences in file
+NUMSEQ=$(grep -c ">" $i)
+##Calculate total sequence length(excluding spaces, gaps "-", and newline)
+TOTAL_SEQ_LENGTH=$(awk '!/>/{gsub(/[- \t\r\n\v\f]/, "", $0); total += length($0)} END {print total}' $i)
+
+##Check if file contains Amino Acid or Nucleotide sequences
+if grep -v ">" $i | grep -q '[RNDEQHILKMFPSWYV]'
+then
+TYPESEQ="Amino Acids"
+else 
+TYPESEQ="Nucleotides"
+fi
+
+echo ====$i $FILETYPE $NUMSEQ $TOTAL_SEQ_LENGTH $TYPESEQ
+done
+
+
