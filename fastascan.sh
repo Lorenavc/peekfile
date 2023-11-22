540686d9490ded54d8c38dc0307531163494b7c2
Lorena <lorenavc@github.com>
2023-11-22 13:16:40 +0100

    fifth commit

--- fastascan.sh
+++ fastascan.sh
@@ -6,13 +6,19 @@ then X=.
 else X=$1
 fi
 
+#Setting N=0 as default if user does not provide second argument
+if [[ -z $2 ]]
+then N=0
+else N=$2
+fi
+
 
 #Finding the number of fasta/fa files
-NUMFILES=$(find $X -type f -name "*.fa" -o -name "*.fasta" | wc -l)
+NUMFILES=$(find "$X" -type f -name "*.fa" -o -name "*.fasta" | wc -l)
 
 ##Checking if there are no fasta files
 if [[ $NUMFILES -eq 0 ]]; then
-echo No fasta/fa files found. Exiting 
+echo No fasta/fa files found. Exiting. 
 exit 1
 fi
 
@@ -20,14 +26,21 @@ echo Number of fasta/fa files: $NUMFILES
 
 
 #Finding the total number of unique fasta IDs
-NUMOFIDS=$(awk '/>/{print $1}' $(find $X -type f -name "*.fa" -o -name "*.fasta") | sort | uniq | wc -l) 
+NUMOFIDS=$(awk '/>/{print $1}' $(find "$X" -type f -name "*.fa" -o -name "*.fasta") | sort | uniq | wc -l) 
 echo Total number of unique fasta IDs: $NUMOFIDS
 echo 
+
+#Checking if N is a positive integer
+if [[ "$N" -lt 0 || "$N" == *.* ]]
+then echo The second argument must be a positive integer. Try again with a positive integer to receive full report.
+exit 2
+fi
+
 echo ====Filename Filetype NumberofSeqs TotalSeqLength SeqType
 echo  
 
 #Iterating through fasta/fa files
-find $X -type f -name "*.fa" -o -name "*.fasta" | while read i; do
+find "$X" -type f -name "*.fa" -o -name "*.fasta" | while read i; do
 
 ##Checking if file is empty
 if [[ ! -s $i ]]
@@ -48,7 +61,7 @@ fi
 
 
 ##Count the number of fasta sequences in file
-NUMSEQ=$(grep -c ">" $i)
+NUMSEQ=$(grep -c ">" "$i")
 if [[ $NUMSEQ -eq 0 ]]
 then echo ====$i
 echo No sequence IDs were found in file. Skipping file...
@@ -60,7 +73,7 @@ fi
 TOTAL_SEQ_LENGTH=$(awk '!/>/{gsub(/[- \t\r\n\v\f]/, "", $0); total += length($0)} END {print total}' $i)
 
 ##Check if file contains Amino Acid or Nucleotide sequences
-if grep -v ">" $i | grep -q '[RNDEQHILKMFPSWYV]'
+if grep -v ">" "$i" | grep -q '[RNDEQHILKMFPSWYV]'
 then
 TYPESEQ="AminoAcids"
 else 
@@ -69,26 +82,21 @@ fi
 
 echo ====$i $FILETYPE $NUMSEQ $TOTAL_SEQ_LENGTH $TYPESEQ
 
-#Setting N=0 as default if user does not provide second argument
-if [[ -z $2 ]]
-then N=0
-else N=$2
-fi
 
-#Printing file lines according to N value
-NUMLINES=$(cat $i | wc -l) 
+
+##Printing file lines according to N value
+NUMLINES=$(wc -l < "$i") 
 if [[ $N -eq 0 ]]
 then 
   echo  
-  continue
 elif [[ $NUMLINES -le $((2*$N)) ]] 
 then 
-  cat $i
+  cat "$i"
   echo
 else
-  head -n $N $i
+  head -n "$N" "$i"
   echo ...
-  tail -n $N $i 
+  tail -n "$N" "$i" 
   echo
 fi
 
